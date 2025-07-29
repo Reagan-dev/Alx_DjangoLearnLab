0b74d829972883d4f8bc39cdbc7815dd2b9f9840
Reagan-dev <reagansimiyu38@gmail.com>
2025-07-29 16:41:07 +0300

    added example form

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Article
 from .models import Book
+from .forms import ExampleForm
 
 # View to list all books, protected by can_view permission
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -31,8 +32,23 @@ def delete_article(request, article_id):
     # Code for deleting article
     pass
 
-Book.objects.filter(title__icontains=user_input)
+# Example of searching books by user input inside a view function
+def search_books(request):
+    user_input = request.GET.get('q', '')
+    books = Book.objects.filter(title__icontains=user_input)
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
-form = BookForm(request.POST)
-if form.is_valid():
-    form.save()
+# Example of handling BookForm inside a view function
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/book_form.html', {'form': form})
+
+def example_form_view(request):
+    form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
