a3f0eb93991ba8902a73d6038c98662bd8c92e0a
Reagan-dev <reagansimiyu38@gmail.com>
2025-07-29 16:05:17 +0300

    did some chandes in views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,31 +2,31 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Article
-from .forms import ArticleForm
+from .models import Book
+
+# View to list all books, protected by can_view permission
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 
-# Create your views here.
-@permission_required('articles.can_view', raise_exception=True)
+@permission_required('bookshelf.can_view', raise_exception=True)
 def article_list(request):
     articles = Article.objects.all()
     return render(request, 'articles/list.html', {'articles': articles})
 
-@permission_required('articles.can_create', raise_exception=True)
-def article_create(request):
-    if request.method == 'POST':
-        form = ArticleForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('article_list')
-    else:
-        form = ArticleForm()
-    return render(request, 'articles/create.html', {'form': form})
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_article(request):
+    # Code for creating article (form logic here)
+    pass
 
-@permission_required('articles.can_edit', raise_exception=True)
-def article_edit(request, pk):
-    article = Article.objects.get(pk=pk)
-    form = ArticleForm(request.POST or None, instance=article)
-    if form.is_valid():
-        form.save()
-        return redirect('article_list')
-    return render(request, 'articles/edit.html', {'form': form, 'article': article})
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_article(request, article_id):
+    # Code for editing article
+    pass
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_article(request, article_id):
+    # Code for deleting article
+    pass
