7c01b339d8cee7867090e09b5675ffc94b0cf969
Reagan-dev <reagansimiyu38@gmail.com>
2025-08-05 15:14:16 +0300

    token authentication

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 from django.urls import include
+from rest_framework.authtoken.views import obtain_auth_token
 # Create a router and register our viewset with it.
 
 router = DefaultRouter()
@@ -11,5 +12,6 @@ urlpatterns = router.urls
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
-    path('', include(router.urls))  # Include the router URLs
+    path('', include(router.urls)),  # Include the router URLs
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # Token authentication endpoint
 ]
