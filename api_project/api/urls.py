9f3162e5d75cb342e54caa869f0c93f831b20e81
Reagan-dev <reagansimiyu38@gmail.com>
2025-08-05 14:32:44 +0300

    modified views to contain all crud operations

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,15 @@
 from .views import BookList
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+from django.urls import include
+# Create a router and register our viewset with it.
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+urlpatterns = router.urls
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('', include(router.urls))  # Include the router URLs
 ]
