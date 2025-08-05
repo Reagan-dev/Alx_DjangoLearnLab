c4bf0f87a15f4cf0fc2e9e6d93eb4d94724da2d6
Reagan-dev <reagansimiyu38@gmail.com>
2025-08-05 15:18:07 +0300

    added permissions

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -128,3 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_AUTHENTICATION_CLASSES = [
     'rest_framework.authentication.TokenAuthentication',
 ]
+
+DEFAULT_PERMISSION_CLASSES = [
+    'rest_framework.permissions.IsAuthenticated',
+]
