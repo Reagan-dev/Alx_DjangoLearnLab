d6636abaf56bd29697356c40c08952c65e775744
Reagan-dev <reagansimiyu38@gmail.com>
2025-08-22 11:59:29 +0300

    urls configurations

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -25,9 +25,9 @@ path('profile/', views.profile, name='profile'),
 path('profile/edit/', views.profile_edit, name='profile_edit'),
 
 # Blog Post URLs
-path('posts/', views.PostListView.as_view(), name='post_list'),
-path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
-path('posts/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
-path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+path('post/', views.PostListView.as_view(), name='post_list'),
+path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
